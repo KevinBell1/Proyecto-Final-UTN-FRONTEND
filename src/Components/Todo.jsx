1c474c99fd434a594cfff415de28fb20ac40aad0
kevin.dev <KevinBell.dev@gmail.com>
2025-01-03 15:24:34 -0300

    bugfix dependencias corruptas

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
