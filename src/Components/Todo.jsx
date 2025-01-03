6483d38c40f78cae3c713275aeedb85688c424be
kevin.dev <KevinBell.dev@gmail.com>
2025-01-03 15:27:11 -0300

    bugfix iconos corruptos

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
 
 
 
@@ -10,8 +8,8 @@ const Todo = ({task, toggleComplete, deleteTodo, editTodo}) => {
         <div className='todo'>
             <p className={`${task.completed ? "completed" : "incompleted"}`} onClick={() => toggleComplete(task.id)} >{task.task}</p>
             <div>
-                <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} onClick={() => editTodo(task.id)}/>
-                <FontAwesomeIcon className="delete-icon" icon={faTrash} onClick={() => deleteTodo(task.id)}/>
+                {/* <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} onClick={() => editTodo(task.id)}/>
+                <FontAwesomeIcon className="delete-icon" icon={faTrash} onClick={() => deleteTodo(task.id)}/> */}
             </div>
         </div>
     )
