fffcfd34f3cfd92a1776d5264380f5d344f6d948
kevin.dev <KevinBell.dev@gmail.com>
2025-01-06 22:54:12 -0300

    bugfix css errores

--- src/Screens/RegisterScreen.jsx
+++ src/Screens/RegisterScreen.jsx
@@ -63,12 +63,8 @@ const RegisterScreen = () => {
         <div className='register-container'>
             <h1>Registrate en Brand name</h1>
 
-            {generalError && (
-                <div className="error-banner">
-                    <p>{generalError}</p>
-                </div>
-            )}
             
+
             <form onSubmit={handleRegister} className='register-box'>
                 <div className='input-container'>
                     <label>Ingresa tu nombre</label>
@@ -95,6 +91,11 @@ const RegisterScreen = () => {
                         className='todo-input'
                     />
                     {errors.email && <p className="error-text">{errors.email.join(", ")}</p>}
+                    {generalError && (
+                <div className="error-banner">
+                    <p>{generalError}</p>
+                </div>
+            )}
                 </div>
                 <div className='input-container'>
                     <label>Ingresa tu contraseña</label>
